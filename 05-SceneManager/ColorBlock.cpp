9b321e129fedaf43a9759fb7b7d49ad88d151d91
Nguyễn Tiến Hoàng <hoangadgj99@gmail.com>
2025-04-08 13:29:13 +0700

    Add DirectionColliable for ColorBlock

--- 05-SceneManager/ColorBlock.cpp
+++ 05-SceneManager/ColorBlock.cpp
@@ -9,5 +9,15 @@ void CColorBlock::Render()
 
 void CColorBlock::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
+	l = x - COLORBLOCK_BBOX_WIDTH / 2;
+	t = y - COLORBLOCK_BBOX_HEIGHT / 2;
+	r = l + COLORBLOCK_BBOX_WIDTH;
+	b = t + COLORBLOCK_BBOX_HEIGHT;
+}
 
+int CColorBlock::IsDirectionColliable(float nx, float ny)
+{
+	if(this->aniID == 81001 || this->aniID == 81002 || this->aniID == 81003 || this->aniID == 82001 || this->aniID == 82002 || this->aniID == 82003 || this->aniID == 83001 || this->aniID == 83002 || this->aniID == 83003 || this->aniID == 84001 || this->aniID == 84002 || this->aniID == 84003)
+		if (nx == 0 && ny == -1) return 1;
+	else return 0;
 }
